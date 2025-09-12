6d64a31d6b743bb43d9218ad7d91c084632c4d44
Juan-Legion-Doan <qdoan21.legion@gmail.com>
2025-09-12 11:32:26 +0700

    config dadbod for linux

--- init.lua
+++ init.lua
@@ -641,6 +641,25 @@ require('lazy').setup({
     end,
   },
   {
+    'tpope/vim-dadbod',
+    lazy = true,
+  },
+  {
+    'kristijanhusak/vim-dadbod-ui',
+    dependencies = { 'tpope/vim-dadbod', 'kristijanhusak/vim-dadbod-completion' },
+    cmd = { 'DBUI', 'DBUIToggle', 'DBUIAddConnection', 'DBUIFindBuffer' },
+    init = function()
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+    keys = {
+      { '<leader>db', '<cmd>DBUIToggle<cr>', desc = 'Toggle Dadbod UI' },
+    },
+  },
+  {
+    'kristijanhusak/vim-dadbod-completion',
+    ft = { 'sql', 'pgsql', 'postgres', 'plsql' },
+  },
+  {
     'kdheepak/lazygit.nvim',
     cmd = 'LazyGit',
     dependencies = {
